4310dc504f0217b0099ddb9bf116806c5b472000
forestseaturtle <fromnewyorkits@live.com>
2015-12-07 07:05:35 -0800

    v0.003b

---
+++ calculators/lifecycle.py
@@ -0,0 +1,20 @@
+# This calculator is going to show you the average lifecycle expectations of
+# organisms in the colony.
+
+#Import Portion
+
+import random
+
+# Initial Defining Portion
+
+family1_1 = random.randint(4, 19)
+family1_2 = random.randint(4, 19)
+family1_3 = random.randint(4, 19)
+family2_1 = random.randint(9, 20)
+family2_2 = random.randint(9, 20)
+family2_3 = random.randint(9, 20)
+
+# Mathematics Portion
+
+family1_addition = family1_1 + family1_2 + family1_3
+family2_addition = family2_1 + family2_2 + family2_3
