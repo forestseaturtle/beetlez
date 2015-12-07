00ac2564ca62fc4f2a9ed2fc6acf5743c07a0c0b
forestseaturtle <fromnewyorkits@live.com>
2015-12-06 23:57:57 -0800

    v0.002b

--- calculators/babies.py
+++ calculators/babies.py
@@ -1,32 +1,32 @@
-## This is going to be a simple test calculator to make sure I can do python correctly still
+## Calculates estimated egg production per female beetle
+print()
+print()
+print("==================")
+print("==================")
+print()
+print()
+print ("This calculator will determine the estimated egg production")
+print ("This is based on total number of female beetles.")
+print ("Calculated using the average egg laying rate.")
+print()
+print()
+print()
 
 ## The input portion
 x = int(input("How many bugs? "))
 
-## The introduction portion
-print()
-print()
-print()
-print ("The mother beetle can lay up to 500 eggs.")
-print ()
-print("Think about what you have just done.")
-print()
-print()
-print()
-print()
-print()
-print("My god.")
-print()
-print()
-print()
-
 ## The calculation portion
 def babe(y):
     babies = x * 500
-    print (babies)
+    return babies
 
 # The results portion
-print (babe(x))
 print()
 print()
-print ("That is a boat load of children!")
+print (babe(x), "", "eggs")
+print()
+print()
+print("==================")
+print("==================")
+print()
+print()
