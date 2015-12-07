8bc691d21a13ac43c262397ce333a9442348282b
forestseaturtle <fromnewyorkits@live.com>
2015-12-07 07:14:23 -0800

    beetlez v0.003b2

--- calculators/lifecycle.py
+++ calculators/lifecycle.py
@@ -18,3 +18,9 @@ family2_3 = random.randint(9, 20)
 
 family1_addition = family1_1 + family1_2 + family1_3
 family2_addition = family2_1 + family2_2 + family2_3
+
+family1 = family1_addition / 3
+family2 = family2_addition / 3
+
+print(family1)
+print(family2)
