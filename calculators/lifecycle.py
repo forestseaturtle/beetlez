cceea6a25bbb411a4cdf6903db7f15dd2673f001
forestseaturtle <fromnewyorkits@live.com>
2015-12-07 14:47:35 -0800

    v0.003a-v2

--- calculators/lifecycle.py
+++ calculators/lifecycle.py
@@ -1,1 +1,15 @@
 # This calculator is going to attempt to calculate the rate at which beetles and eggs will be produced by the colony
+
+# Introduction
+
+print ()
+print ()
+print ("Please enter the amount of worms you are starting with: ")
+print ()
+print ()
+
+# Initial Worm Entry
+
+worm0 = int(input("How many worms you got - "))
+print ()
+print ()
