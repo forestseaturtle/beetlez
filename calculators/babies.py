725e8e9053ccecf2fa3cad2007825b0e29faa890
forestseaturtle <fromnewyorkits@live.com>
2015-12-06 23:45:47 -0800

    v0.001

---
+++ calculators/babies.py
@@ -0,0 +1,32 @@
+## This is going to be a simple test calculator to make sure I can do python correctly still
+
+## The input portion
+x = int(input("How many bugs? "))
+
+## The introduction portion
+print()
+print()
+print()
+print ("The mother beetle can lay up to 500 eggs.")
+print ()
+print("Think about what you have just done.")
+print()
+print()
+print()
+print()
+print()
+print("My god.")
+print()
+print()
+print()
+
+## The calculation portion
+def babe(y):
+    babies = x * 500
+    print (babies)
+
+# The results portion
+print (babe(x))
+print()
+print()
+print ("That is a boat load of children!")
